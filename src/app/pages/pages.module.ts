a67a348a3e5491417ec5d6dd97481e5ae624564d
srx32 <kdavidmensah@gmail.com>
2023-07-13 14:13:44 +0000

    Removed dashboard component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,43 +1,36 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { FlatpickrModule } from 'angularx-flatpickr';
-import { CountToModule } from 'angular-count-to';
-import { NgApexchartsModule } from 'ng-apexcharts';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-import { SimplebarAngularModule } from 'simplebar-angular';
+import { FlatpickrModule } from "angularx-flatpickr";
+import { CountToModule } from "angular-count-to";
+import { NgApexchartsModule } from "ng-apexcharts";
+import { LeafletModule } from "@asymmetrik/ngx-leaflet";
+import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
+import { SimplebarAngularModule } from "simplebar-angular";
 
 // Swiper Slider
-import { SwiperModule } from 'ngx-swiper-wrapper';
-import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
-import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { SwiperModule } from "ngx-swiper-wrapper";
+import { SWIPER_CONFIG } from "ngx-swiper-wrapper";
+import { SwiperConfigInterface } from "ngx-swiper-wrapper";
 
-import { LightboxModule } from 'ngx-lightbox';
+import { LightboxModule } from "ngx-lightbox";
 
 // Load Icons
-import { defineLordIconElement } from 'lord-icon-element';
-import lottie from 'lottie-web';
+import { defineLordIconElement } from "lord-icon-element";
+import lottie from "lottie-web";
 
 // Pages Routing
 import { PagesRoutingModule } from "./pages-routing.module";
 import { SharedModule } from "../shared/shared.module";
-// import { WidgetModule } from '../shared/widget/widget.module';
-import { DashboardComponent } from './dashboards/dashboard/dashboard.component';
-import { DashboardsModule } from "./dashboards/dashboards.module";
-import { SecretariatModule } from './secretariat/secretariat.module';
-// import { AppsModule } from "./apps/apps.module";
-// import { EcommerceModule } from "./ecommerce/ecommerce.module";
+import { SecretariatModule } from "./secretariat/secretariat.module";
 
 const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
-  direction: 'horizontal',
-  slidesPerView: 'auto'
+  direction: "horizontal",
+  slidesPerView: "auto",
 };
 
 @NgModule({
-  declarations: [
-    DashboardComponent,
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     FlatpickrModule.forRoot(),
@@ -48,23 +41,19 @@ const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
     SimplebarAngularModule,
     PagesRoutingModule,
     // SharedModule,
-    // WidgetModule,
     SwiperModule,
     LightboxModule,
-    DashboardsModule,
-    // AppsModule,
-    // EcommerceModule,
-    SecretariatModule
+    SecretariatModule,
   ],
   providers: [
     {
       provide: SWIPER_CONFIG,
-      useValue: DEFAULT_SWIPER_CONFIG
-    }
+      useValue: DEFAULT_SWIPER_CONFIG,
+    },
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class PagesModule { 
+export class PagesModule {
   constructor() {
     defineLordIconElement(lottie.loadAnimation);
   }
